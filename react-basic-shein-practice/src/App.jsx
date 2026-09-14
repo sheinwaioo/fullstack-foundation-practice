f763d379958ab080861903f39742578275cd16f1
Shein Wai Oo <sheinwaioo9@gmail.com>
2026-09-14 14:12:21 +0800

    new tests

--- react-basic-shein-practice/src/App.jsx
+++ react-basic-shein-practice/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useState} from "react";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import Greeting, {color,age} from"./components/Greeting";
+import "./App.css";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <section id="center">
+        <Greeting/>
+        {color} {age}
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
@@ -116,7 +121,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ react-basic-shein-practice/src/components/Greeting.jsx
@@ -0,0 +1,15 @@
+function Greeting() {
+  return (
+  <>
+    <h1>Hello, Singapore</h1>
+    <p>This is testing</p>
+  </>
+  );
+}
+
+const color = "yellow";
+const age = 10;
+
+export {color, age};
+
+export default Greeting;
